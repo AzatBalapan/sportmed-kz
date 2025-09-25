cb2e9ba8decc0c4880928115298f13d6cc72304b
uwiieki <161056439+uwiieki@users.noreply.github.com>
2025-09-25 14:35:24 +0500

    Update News.tsx

--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -19,14 +19,6 @@ const News: React.FC = () => {
 
     // News data config (newest first)
     const newsConfig = [
-     {
- id: 'new-article-10',
-        image: '/news/10/photoes/1.jpeg',
-        alt: language === 'ru' ? 'Айдос Султангали - чемпион мира!' : language === 'kz' ? 'Айдос Сұлтанғали - әлем чемпионы!' : 'Aidos Sultangali is the champion of the world!',
-        title: language === 'ru' ? 'Айдос Султангали - чемпион мира!' : language === 'kz' ? 'Айдос Сұлтанғали - әлем чемпионы!' : 'Aidos Sultangali is the champion of the world!',
-        textPath: `/news/${language === 'ru' ? '10/10_rus.txt' : '10/10_kaz.txt'}`,
-        onClick: () => navigate('/news/new-article-10'),
-      },
       {
         id: 'new-article-9',
         image: language === 'ru' ? '/news/9/rus/photos/1.jpg' : '/news/9/kaz/photos/1.jpg',
