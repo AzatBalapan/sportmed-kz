1310baf0a2972ba4eb792687932c328643b72753
uwiieki <161056439+uwiieki@users.noreply.github.com>
2025-10-31 10:01:21 +0500

    Update SocialLinks.tsx

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -21,6 +21,14 @@ const SocialLinks: React.FC = () => {
       >
         <Instagram size={20} />
       </a>
+     <a
+        href="www.tiktok.com/@sportmedortalyq.astana"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-black hover:bg-gray-800 text-white p-3 rounded-full shadow-lg transition-all duration-300 mt-2"
+        aria-label="TikTok"
+     >
+        <TikTok size={20} />
     </div>
   );
 };
