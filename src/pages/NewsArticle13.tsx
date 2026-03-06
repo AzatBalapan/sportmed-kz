907c54d6e3ba2314dd3c9cc2fa9fed5f8cac5080
uwiieki <161056439+uwiieki@users.noreply.github.com>
2026-03-06 22:08:42 +0500

    Update NewsArticle13.tsx

--- src/pages/NewsArticle13.tsx
+++ src/pages/NewsArticle13.tsx
@@ -39,11 +39,11 @@ const NewsArticle13: React.FC = () => {
   const pdfFiles = language === 'ru'
     ? [
         { label: 'Протокол №1', path: '/news/13/Протокол.pdf' },
-        { label: 'График собеседования', path: '/news/13/Сухбат_русс.pdf' },
+        { label: 'График собеседования', path: '/news/13/Сухбатрусс.pdf' },
       ]
     : [
         { label: 'Хаттама №1', path: '/news/13/Хаттама.pdf' },
-        { label: 'Әңгімелесу кестесі', path: '/news/13/Сухбат_каз.pdf' },
+        { label: 'Әңгімелесу кестесі', path: '/news/13/Сухбатказ.pdf' },
       ];
 
   const docsTitle = language === 'ru' ? 'Документы' : 'Құжаттар';
