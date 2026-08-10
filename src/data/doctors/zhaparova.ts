f5ac6af4341b98e886a0fddbc2a7a828b69b1995
uwiieki <161056439+uwiieki@users.noreply.github.com>
2026-08-10 09:31:33 +0500

    Update doctor image path for zhaparova

--- src/data/doctors/zhaparova.ts
+++ src/data/doctors/zhaparova.ts
@@ -9,7 +9,7 @@ export const zhaparova = {
     kz: 'Дәрігер-кардиолог',
   },
   department: 'specialists',
-  image: '/team_photoes/aizhan.png',
+  image: '/team_photoes/new/aizhan.png',
   experience: {
     ru: 'Более 20 лет',
     kz: '20 жылдан астам',
